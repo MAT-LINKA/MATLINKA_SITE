f3721625f7cbdcce0f92469c6a1e8b0980e53381
Sedhumolm <sedhumol.m@erstechnology.com>
2025-07-29 20:12:17 +0200

    BUG FIXING

--- app/page.tsx
+++ app/page.tsx
@@ -97,19 +97,19 @@ export default function Home() {
           {/* Navigation Arrows */}
           <button
             onClick={prevSlide}
-            className="absolute left-4 top-1/2 transform -translate-y-1/2 bg-white/20 hover:bg-white/30 text-white p-3 rounded-full backdrop-blur-sm cursor-pointer"
+            className="absolute left-4 top-1/3 transform -translate-y-1/2 bg-white/20 hover:bg-white/30 text-white p-2 rounded-full backdrop-blur-sm cursor-pointer"
           >
             <i className="ri-arrow-left-line w-6 h-6 flex items-center justify-center"></i>
           </button>
           <button
             onClick={nextSlide}
-            className="absolute right-4 top-1/2 transform -translate-y-1/2 bg-white/20 hover:bg-white/30 text-white p-3 rounded-full backdrop-blur-sm cursor-pointer"
+            className="absolute right-4 top-1/3 transform -translate-y-1/2 bg-white/20 hover:bg-white/30 text-white p-2 rounded-full backdrop-blur-sm cursor-pointer"
           >
             <i className="ri-arrow-right-line w-6 h-6 flex items-center justify-center"></i>
           </button>
 
           {/* Slide Indicators */}
-          <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 flex space-x-2">
+          <div className="absolute bottom-8 left-1/ transform -translate-x-1/2 flex space-x-2">
             {slides.map((_, index) => (
               <button
                 key={index}
